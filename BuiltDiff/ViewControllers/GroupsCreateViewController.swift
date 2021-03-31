15114109f1e0064bf49bfe1b1fa8099902ffd619
Chris <vingilot66@hotmail.com>
2021-03-31 18:16:15 -0400

    Groups work

--- BuiltDiff/ViewControllers/GroupsCreateViewController.swift
+++ BuiltDiff/ViewControllers/GroupsCreateViewController.swift
@@ -13,13 +13,27 @@ class GroupsCreateViewController: UIViewController {
     @IBOutlet var BackgroundBox: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var groupNameInput: UITextField!
-    
+    @IBOutlet var ImageSelector: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imageView.isHidden = true
         BackgroundBox.layer.cornerRadius = 25.0
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func chooseImage(sender: AnyObject) {
+        imageView.isHidden = false
+        switch ImageSelector.selectedSegmentIndex {
+        case 0:
+          imageView.image = UIImage(named: "SoccerTeam")
+        case 1:
+          imageView.image = UIImage(named: "GymGroup")
+        case 2:
+          imageView.image = UIImage(named: "RunningGroup")
+        default:
+          imageView.image = UIImage(named: "SoccerTeam")
+        }
     }
     
     @IBAction func onCreateGroup(_ sender: Any) {
