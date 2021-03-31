340924fb6f00cb892e4b63679cf28c5fed9d5305
Chris <vingilot66@hotmail.com>
2021-03-31 14:01:24 -0400

    Groups Page Work
    Added UI and create group screen

---
+++ BuiltDiff/ViewControllers/GroupsCreateViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GroupsCreateViewController.swift
+//  BuiltDiff
+//
+//  Created by Christopher Reynolds on 2021-03-31.
+//
+
+import UIKit
+
+class GroupsCreateViewController: UIViewController {
+
+    @IBOutlet var BackgroundBox: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        BackgroundBox.layer.cornerRadius = 25.0
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
