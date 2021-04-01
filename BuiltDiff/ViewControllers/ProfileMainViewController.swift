2486bc2ba5ebd16ec55c21cdfc054e8fb9c84b5b
Azmeer Hussain <azmeerh1998@gmail.com>
2021-04-01 14:10:47 -0400

    Created the avatar customization skeleton layout, still yet to be implemented fully.

--- BuiltDiff/ViewControllers/ProfileMainViewController.swift
+++ BuiltDiff/ViewControllers/ProfileMainViewController.swift
@@ -8,13 +8,12 @@
 import UIKit
 
 class ProfileMainViewController: UIViewController {
-
+    
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var backgroundFill: UIImageView!
     @IBOutlet weak var profileName: UILabel!
     @IBOutlet weak var backgroundMedal: UIImageView!
     @IBOutlet weak var exercisesBackground: UIImageView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +24,34 @@ class ProfileMainViewController: UIViewController {
         profilePicture.layer.masksToBounds = true
         backgroundMedal.layer.cornerRadius = 30
         exercisesBackground.layer.cornerRadius = 30
+        
+        let gesture = UIGestureRecognizer(target: self, action: #selector(didTapChangeProfilePicture))
+        profilePicture.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func didTapChangeProfilePicture(){
+        presentPhotoActionSheet()
+    }
+}
+
+extension ProfileMainViewController : UIImagePickerControllerDelegate{
+    
+    func presentPhotoActionSheet(){
+        let actionSheet = UIAlertController(title: "Profile Picture", message: "How would you like to select a picture?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { _ in
+            
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { _ in
+            
+        }))
+        
+        present(actionSheet, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
     }
 }
