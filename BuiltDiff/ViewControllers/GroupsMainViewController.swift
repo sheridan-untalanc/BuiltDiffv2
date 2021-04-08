ef75ea1c6365411ce861f6a6da2ea450db35030e
Chris <vingilot66@hotmail.com>
2021-04-08 10:57:28 -0400

    Group TableView
    Added table view for displaying group informatiion

--- BuiltDiff/ViewControllers/GroupsMainViewController.swift
+++ BuiltDiff/ViewControllers/GroupsMainViewController.swift
@@ -7,23 +7,47 @@
 
 import UIKit
 
-class GroupsMainViewController: UIViewController {
 
+let names = [
+    "Soccer Team",
+    "Gym Group",
+    "Running Club"
+]
+let images = [
+    "SoccerTeam",
+    "GymGroup",
+    "RunningGroup"
+]
+
+class GroupsMainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ("cellGroup"), for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        cell.imageView?.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    func tableView( _ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print ("You tapped me")
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
