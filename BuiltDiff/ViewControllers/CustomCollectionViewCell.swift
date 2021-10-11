a91a81e818d3e1f77c14d297cf3dd67764b8a15a
Azmeer Hussain <azmeerh1998@gmail.com>
2021-10-11 17:59:01 -0400

    configured the collection view to add necessary features for exercise page

--- BuiltDiff/ViewControllers/CustomCollectionViewCell.swift
+++ BuiltDiff/ViewControllers/CustomCollectionViewCell.swift
@@ -11,39 +11,53 @@ class CustomCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "CustomCollectionViewCell"
     
-    private let myImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "exercise1")
-        imageView.contentMode = .scaleAspectFill
-        imageView.backgroundColor = .yellow
-        imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 10
-        return imageView
+    private let myIcon: UIImageView = {
+        let icon = UIImageView()
+//        icon.image = testArray[indexPath.row]
+        icon.contentMode = .scaleAspectFill
+        icon.backgroundColor = .red
+//        icon.clipsToBounds = true
+        icon.layer.cornerRadius = 10
+        return icon
     }()
     
-    private let myLabel: UILabel = {
-        let label = UILabel()
-        label.text = "test"
-//        label.backgroundColor = .green
-        label.font = UIFont.boldSystemFont(ofSize: 15)
-        label.textAlignment = .left
-        return label
+    private let myView: UIImageView = {
+        let cardView = UIImageView()
+        cardView.contentMode = .scaleAspectFill
+        cardView.backgroundColor = .yellow
+        cardView.clipsToBounds = true
+        cardView.layer.cornerRadius = 10
+//        cardView.layer.shadowOpacity = 1
+//        cardView.layer.shadowColor = UIColor.black.cgColor
+//        cardView.layer.shadowOffset = CGSize(width: 10, height: 10)
+//        cardView.layer.shadowRadius = 10
+        return cardView
     }()
     
-    public let myButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Button", for: .normal)
-        button.backgroundColor = .green
-        return button
+    private let myType: UILabel = {
+        let type = UILabel()
+        type.text = "  Type"
+        type.backgroundColor = .green
+        type.font = UIFont.boldSystemFont(ofSize: 15)
+        type.textAlignment = .left
+        return type
     }()
     
+    private let myDate: UILabel = {
+        let date = UILabel()
+        date.text = "  Type"
+        date.backgroundColor = .green
+        date.font = UIFont.boldSystemFont(ofSize: 15)
+        date.textAlignment = .right
+        return date
+    }()
     
     override init(frame: CGRect){
         super.init(frame: frame)
-//        contentView.backgroundColor = .red
-        contentView.addSubview(myLabel)
-        contentView.addSubview(myImageView)
-        contentView.addSubview(myButton)
+        contentView.addSubview(myView)
+        myView.addSubview(myIcon)
+        myView.addSubview(myType)
+        myView.addSubview(myDate)
         contentView.clipsToBounds = true
     }
     
@@ -53,17 +67,15 @@ class CustomCollectionViewCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        myLabel.frame = CGRect(x: 5, y: contentView.frame.size.height-50, width: contentView.frame.size.width-10, height: 50)
-        myImageView.frame = CGRect(x: 5, y: 0, width: contentView.frame.size.width-10, height: contentView.frame.size.height-50)
-        myButton.frame = CGRect(x: 5, y: contentView.frame.size.height-50, width: contentView.frame.size.width-10, height: 50)
+        myType.frame = CGRect(x: 70, y: 10, width: 100, height: 50)
+        myDate.frame = CGRect(x: 250, y: 10, width: 100, height: 50)
+        myIcon.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
+        myView.frame = CGRect(x: 10, y: 0, width: contentView.frame.size.width-20, height: contentView.frame.size.height)
     }
     
-    public func configure(label: String){
-        myLabel.text = label
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        myLabel.text = nil
+    public func configure(image: UIImage, label: String, date: String){
+        myIcon.image = image
+        myType.text = label
+        myDate.text = date
     }
 }
--- BuiltDiff/ViewControllers/ExerciseViewController.swift
+++ BuiltDiff/ViewControllers/ExerciseViewController.swift
@@ -18,62 +18,60 @@ class ExerciseViewController: UIViewController, UICollectionViewDelegate, UIColl
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.authorizeHealthKitInApp()
-        
-        let workoutPredicate = HKQuery.predicateForWorkouts(with: .walking)
-          let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate,
-                                                ascending: true)
-        
-        let query = HKSampleQuery(
-          sampleType: .workoutType(),
-          predicate: nil,
-          limit: 10,
-          sortDescriptors: [sortDescriptor]) { (query, samples, error) in
-            DispatchQueue.main.async {
-              guard
-                let samples = samples as? [HKWorkout],
-                error == nil
-                else {
-                  return
-              }
-                let sample = samples[2]
-                
-                let distance = Double((sample.totalDistance!.doubleValue(for: HKUnit.meter())))
-//                self.lblDistance.text = "\(String((distance/1000).rounded(.awayFromZero))) km"
-                
-                let calories = Double((sample.totalEnergyBurned!.doubleValue(for: HKUnit.largeCalorie())))
-                
-                var exerciseType = ""
-                switch sample.workoutActivityType.rawValue{
-                case 46:
-                    exerciseType = "Swimming"
-                case 13:
-                    exerciseType = "Cycling"
-                case 52:
-                    exerciseType = "Walking"
-                default:
-                    exerciseType = "Generic Workout"
-                }
-                
-                func secondsToHoursMinutesSeconds (seconds : Int) -> (Int, Int, Int) {
-                  return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
-                }
-                let (h,m,s) = secondsToHoursMinutesSeconds(seconds: Int(sample.duration))
-                print(calories)
-                print("\(String((distance/1000).rounded(.awayFromZero))) km")
-              
-            }
-          }
-
-        HKHealthStore().execute(query)
-        
-        self.authorizeHealthKitInApp()
+//        self.authorizeHealthKitInApp()
+//
+//        let workoutPredicate = HKQuery.predicateForWorkouts(with: .walking)
+//        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate,
+//                                                ascending: true)
+//
+//        let query = HKSampleQuery(
+//          sampleType: .workoutType(),
+//          predicate: nil,
+//          limit: 10,
+//          sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+//            DispatchQueue.main.async {
+//              guard
+//                let samples = samples as? [HKWorkout],
+//                error == nil
+//                else {
+//                  return
+//              }
+//                let sample = samples[2]
+//
+//                let distance = Double((sample.totalDistance!.doubleValue(for: HKUnit.meter())))
+////                self.lblDistance.text = "\(String((distance/1000).rounded(.awayFromZero))) km"
+//
+//                let calories = Double((sample.totalEnergyBurned!.doubleValue(for: HKUnit.largeCalorie())))
+//
+//                var exerciseType = ""
+//                switch sample.workoutActivityType.rawValue{
+//                case 46:
+//                    exerciseType = "Swimming"
+//                case 13:
+//                    exerciseType = "Cycling"
+//                case 52:
+//                    exerciseType = "Walking"
+//                default:
+//                    exerciseType = "Generic Workout"
+//                }
+//
+//                func secondsToHoursMinutesSeconds (seconds : Int) -> (Int, Int, Int) {
+//                  return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+//                }
+//                let (h,m,s) = secondsToHoursMinutesSeconds(seconds: Int(sample.duration))
+//                print(calories)
+//                print("\(String((distance/1000).rounded(.awayFromZero))) km")
+//
+//            }
+//          }
+//
+//        HKHealthStore().execute(query)
+//
+//        self.authorizeHealthKitInApp()
 
         // Do any additional setup after loading the view.
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.minimumInteritemSpacing = 1
-        layout.minimumLineSpacing = 1
         layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         layout.itemSize = CGSize(width: (view.frame.size.width),
                                  height: (view.frame.size.height/3.5))
@@ -112,24 +110,39 @@ class ExerciseViewController: UIViewController, UICollectionViewDelegate, UIColl
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 4
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: indexPath) as! CustomCollectionViewCell
-//        cell.contentView.backgroundColor = .blue
-        cell.configure(label: "Exercise \(indexPath.row)")
-        cell.myButton.tag = indexPath.row
-        cell.myButton.addTarget(self, action: #selector(testMethod), for: .touchUpInside)
+        
+        var iconArray: [UIImage] = [
+            UIImage(named: "runningIcon")!,
+            UIImage(named: "runningIcon")!,
+            UIImage(named: "runningIcon")!,
+            UIImage(named: "swimmingIcon")!
+        ]
+        
+        var typeArray: [String] = [
+            "walking",
+            "walking",
+            "walking",
+            "swimming"
+        ]
+        
+        var dateArray: [String] = [
+            "11/10/2021",
+            "11/10/2021",
+            "11/10/2021",
+            "10/10/2021"
+        ]
+        
+        cell.configure(image: iconArray[indexPath.row], label: typeArray[indexPath.row], date: dateArray[indexPath.row])
         return cell
     }
     
-    @objc func testMethod(){
-        
-    }
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        //print("User tapped on item \(indexPath.row)")
+        print("User tapped on item \(indexPath.row)")
 //        print(colories)
     }
 }
