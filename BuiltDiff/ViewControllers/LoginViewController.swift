1b97824f4e90a8c1418100505f74bcdf4de5725f
Azmeer Hussain <azmeerh1998@gmail.com>
2021-10-14 14:18:05 -0400

    Created segue method for code to put inputted into

--- BuiltDiff/ViewControllers/LoginViewController.swift
+++ BuiltDiff/ViewControllers/LoginViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginTapped(_ sender: Any) {
+        //for christian
+        
+        
+        self.performSegue(withIdentifier: "loginSegue", sender: self)
     }
-    */
-
+    
 }
