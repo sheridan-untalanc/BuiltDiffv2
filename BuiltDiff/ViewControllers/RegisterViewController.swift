7f7ebc247c2ca67fe837fb569be8e7d38283884c
Azmeer Hussain <azmeerh1998@gmail.com>
2021-10-14 14:33:21 -0400

    Fixed the error checking with create account page, has full functionality now

--- BuiltDiff/ViewControllers/RegisterViewController.swift
+++ BuiltDiff/ViewControllers/RegisterViewController.swift
@@ -9,50 +9,36 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
-//    @IBOutlet weak var emailInput: UITextField!
-//    @IBOutlet weak var usernameInput: UITextField!
-//    @IBOutlet weak var passwordInput: UITextField!
-//    @IBOutlet weak var passwordConfirmInput: UITextField!
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var passwordConfirmInput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-//    @IBAction func onRegister(_ sender: Any) {
-//        if (passwordInput.text != passwordConfirmInput.text){
-//            let alert = UIAlertController(title: "Error!", message: "Passwords do not match", preferredStyle: .alert)
-//            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
-//            self.present(alert, animated: true, completion: nil)
-//            return
-//        }
-//
-//        guard let email = emailInput.text, !email.isEmpty,
-//              let username = usernameInput.text, !username.isEmpty,
-//              let password = passwordInput.text, !password.isEmpty else {
-//
-//            let alert = UIAlertController(title: "Error!", message: "One or more of the fields is empty.", preferredStyle: .alert)
-//            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
-//            self.present(alert, animated: true, completion: nil)
-//            return
-//        }
-//
-//        FirebaseAccessLayer.Register(username: username, email: email, password: password)
-//
-//        }
-//
-//    }
-    
-    /*
-    // MARK: - Navigation
+    @IBAction func onRegister(_ sender: Any) {
+        if (passwordInput.text != passwordConfirmInput.text){
+            let alert = UIAlertController(title: "Error!", message: "Passwords do not match", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        guard let email = emailInput.text, !email.isEmpty,
+              let username = usernameInput.text, !username.isEmpty,
+              let password = passwordInput.text, !password.isEmpty else {
+
+            let alert = UIAlertController(title: "Error!", message: "One or more of the fields is empty.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+
+        FirebaseAccessLayer.Register(username: username, email: email, password: password)
+
+        }
 
 }
-
