681193339cdab962672236c9521e9ccdbe8db58b
Azmeer Hussain <azmeerh1998@gmail.com>
2021-10-15 22:48:00 -0400

    Finalized UI for create group and join group

--- BuiltDiff/ViewControllers/GroupCustomCollectionViewCell.swift
+++ BuiltDiff/ViewControllers/GroupCustomCollectionViewCell.swift
@@ -11,16 +11,16 @@ class GroupCustomCollectionViewCell: UICollectionViewCell {
     
     static let identifier = "GroupCustomCollectionViewCell"
         
-//        private let myIcon: UIImageView = {
-//            let icon = UIImageView()
-//    //        icon.image = testArray[indexPath.row]
-//            icon.contentMode = .scaleAspectFill
-//            icon.backgroundColor = .red
-//    //        icon.clipsToBounds = true
-//            icon.layer.cornerRadius = 10
-//            return icon
-//        }()
-    
+        private let myIcon: UIImageView = {
+            let icon = UIImageView()
+            icon.image = testArray[indexPath.row]
+            icon.contentMode = .scaleAspectFill
+            icon.backgroundColor = .red
+            icon.clipsToBounds = true
+            icon.layer.cornerRadius = 10
+            return icon
+        }()
+
     private let myView: UIImageView = {
         let cardView = UIImageView()
         cardView.contentMode = .scaleAspectFill
@@ -43,54 +43,54 @@ class GroupCustomCollectionViewCell: UICollectionViewCell {
         return button
     }()
 
-    
+
     private let groupName: UILabel = {
         let name = UILabel()
         name.text = "Group 1"
-//            name.backgroundColor = .green
+        name.backgroundColor = .green
         name.font = UIFont.boldSystemFont(ofSize: 25)
         name.textAlignment = .left
         return name
     }()
-    
+
     private let groupMembers: UILabel = {
         let members = UILabel()
         members.text = ""
-//            members.backgroundColor = .green
+        members.backgroundColor = .green
         members.font = UIFont.boldSystemFont(ofSize: 10)
         members.textAlignment = .left
         return members
     }()
-    
+
     override init(frame: CGRect){
         super.init(frame: frame)
         contentView.addSubview(myView)
-//            myView.addSubview(myIcon)
+        myView.addSubview(myIcon)
         myView.addSubview(groupName)
         myView.addSubview(groupMembers)
         myView.addSubview(joinButton)
         contentView.clipsToBounds = true
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         groupName.frame = CGRect(x: 10, y: 25, width: 200, height: 50)
         groupMembers.frame = CGRect(x: 10, y: 45, width: 100, height: 50)
         joinButton.frame = CGRect(x: 290, y: 35, width: 60, height: 40)
-//            myIcon.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
+        myIcon.frame = CGRect(x: 10, y: 10, width: 50, height: 50)
         myView.frame = CGRect(x: 10, y: 0, width: contentView.frame.size.width-20, height: contentView.frame.size.height)
     }
-    
-//        public func configure(image: UIImage, label: String, date: String){
-//            myIcon.image = image
-//            groupName.text = label
-//            groupMembers.text = date
-//        }
-    
+
+        public func configure(image: UIImage, label: String, date: String){
+            myIcon.image = image
+            groupName.text = label
+            groupMembers.text = date
+        }
+
     public func configure(label: String, members: String){
         groupName.text = label
         groupMembers.text = members
--- BuiltDiff/ViewControllers/SecondGroupViewController.swift
+++ BuiltDiff/ViewControllers/SecondGroupViewController.swift
@@ -9,16 +9,69 @@ import UIKit
 
 class SecondGroupViewController: UIViewController {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var digitCodeLabel: UITextField!
+    @IBOutlet weak var joinButton: UIButton!
+    
     var groupNameArray: [String] = [
-            "Deadlift Squad",
-            "Running Club",
-            "Swim Team",
-            "Bike Crew",
-            "Soccer FC"]
+            "Sheridan Club",
+            "Milton Team",
+            "McMaster Squad",
+            "Band of Brothers",
+            "Raging Bull"]
+    
+    var groupMemberArray: [String] = ["5","10","25","3","6"]
+    
+    var groupDateArray: [String] = [
+            "01/10/2021",
+            "02/10/2021",
+            "03/10/2021",
+            "04/10/2021",
+            "05/10/2021"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
         
+    }
+    
+    @IBAction func joinButtonTapped(_ sender: Any) {
+        if digitCodeLabel.text == "" {
+            let alert = UIAlertController(title: "Error!", message: "Please enter a valid 6 digit code", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        else{
+            print("join button tapped")
+        }
+    }
+    
+}
+
+extension SecondGroupViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "JoinGroupCollectionViewCell", for: indexPath) as! JoinGroupCollectionViewCell
+        cell.layer.cornerRadius = 10
+        
+        cell.configure(name: groupNameArray[indexPath.row], members: "\(groupMemberArray[indexPath.row]) members", date: groupDateArray[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("works")
+    }
+}
+
+extension SecondGroupViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 350, height: 110)
     }
 }
