b7300c887d1f82c8ebdb0abdad7e624d96bcb643
Azmeer Hussain <azmeerh1998@gmail.com>
2021-11-15 19:14:51 -0500

    created different ui skeletons for displaying group exercises and other stats

---
+++ BuiltDiff/ViewControllers/GroupViewControllers/CreateExerciseCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CreateExerciseCollectionViewCell.swift
+//  BuiltDiff
+//
+//  Created by Azmeer Hussain on 2021-11-15.
+//
+
+import UIKit
+
+class CreateExerciseCollectionViewCell: UICollectionViewCell {
+    static let identifier = "CreateExerciseCollectionViewCell"
+}
--- BuiltDiff/ViewControllers/GroupViewControllers/GroupDetailsViewController.swift
+++ BuiltDiff/ViewControllers/GroupViewControllers/GroupDetailsViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 class GroupDetailsViewController: UIViewController {
     @IBOutlet weak var groupImage: UIImageView!
     @IBOutlet weak var groupTitle: UILabel!
-    @IBOutlet weak var groupMemberCount: UILabel!
     @IBOutlet weak var groupDescriptionLabel: UILabel!
     @IBOutlet weak var groupBackButton: UIButton!
+    @IBOutlet weak var groupExerciseCollectionView: UICollectionView!
     
     var group: Group? = nil
     
@@ -21,9 +21,12 @@ class GroupDetailsViewController: UIViewController {
         // Do any additional setup after loading the view.
         groupTitle.text = group?.GroupName
         groupDescriptionLabel.text = group?.GroupDescription
-        groupImage.layer.cornerRadius = 50
+        groupImage.layer.cornerRadius = 75
         groupImage.layer.masksToBounds = true
         groupBackButton.layer.cornerRadius = 20
+        groupExerciseCollectionView.delegate = self
+        groupExerciseCollectionView.dataSource = self
+        groupExerciseCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
         Task.init{
             FirebaseAccessLayer.GetGroupImage(ownerUid: group!.GroupOwner, completion: { image in
                 DispatchQueue.main.async{
@@ -37,4 +40,36 @@ class GroupDetailsViewController: UIViewController {
         performSegue(withIdentifier: "unwindToMyGroups", sender: self)
         }
 
+}
+
+extension GroupDetailsViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        var testArray = [0,1,0,1,0]
+        if testArray[indexPath.row] == 0{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GroupDetailsCollectionViewCell", for: indexPath) as! GroupDetailsCollectionViewCell
+            cell.layer.cornerRadius = 10
+            return cell
+        }else{
+            let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "CreateExerciseCollectionViewCell", for: indexPath) as! CreateExerciseCollectionViewCell
+            cell2.layer.cornerRadius = 10
+            return cell2
+        }
+        
+//        cell.configure(name: groups[indexPath.row].GroupName, description: groups[indexPath.row].GroupDescription)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("works")
+    }
+}
+
+extension GroupDetailsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 350, height: 110)
+    }
 }
