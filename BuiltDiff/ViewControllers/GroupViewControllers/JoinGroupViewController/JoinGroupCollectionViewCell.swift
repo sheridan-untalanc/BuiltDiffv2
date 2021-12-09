b14d6d6e45ed381adf65cb452a79649e2a075d99
ChrisDReynolds <ChrisDReynolds@outlook.com>
2021-12-08 23:33:49 -0500

    File Restructuring

--- BuiltDiff/ViewControllers/GroupViewControllers/JoinGroupViewController/JoinGroupCollectionViewCell.swift
+++ BuiltDiff/ViewControllers/GroupViewControllers/JoinGroupViewController/JoinGroupCollectionViewCell.swift
@@ -13,12 +13,6 @@ class JoinGroupCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var groupDescription: UILabel!
     @IBOutlet weak var groupDate: UILabel!
     
-//    func configure(name: String, members: String, date: String){
-//        groupName.text = name
-//        groupMemberCount.text = members
-//        groupDate.text = date
-//    }
-    
     func configure(name: String, description: String){
         groupName.text = name
         groupDescription.text = description
